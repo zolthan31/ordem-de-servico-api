de78c471913d0cadac9a0c951caffefa1501f320
zolthan31 <dhb.tecnologia@outlook.com>
2023-06-29 14:52:56 -0300

    Teste para a classe principal

---
+++ src/test/java/io/com/zolthan31/resources/OrdemDeServicoResourceTest.java
@@ -0,0 +1,14 @@
+package io.com.zolthan31.resources;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrdemDeServicoResourceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/io/com/zolthan31/resources/TecnicoResourceTest.java
@@ -0,0 +1,14 @@
+package io.com.zolthan31.resources;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TecnicoResourceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
